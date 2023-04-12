340f15bfd4be5089fe1347496aa8af9b8760340c
Jakob <jakst@itu.dk>
2023-04-12 11:33:54 +0200

    Added Types from codejudge submission

---
+++ ScrabbleTemplate/ScrabbleBot/Types.fs
@@ -0,0 +1,17 @@
+module internal ScrabbleBot.Types
+
+open StateMonad
+    
+    type coord  = int * int 
+    type word   = (char * int) list
+    
+    type squareFun = word -> int -> int -> Result<int, Error>
+    type square = Map<int, squareFun>
+    
+    type boardFun = coord -> Result<square option, Error>
+    
+    type board = {
+        center        : coord
+        defaultSquare : square
+        squares       : boardFun
+    }
